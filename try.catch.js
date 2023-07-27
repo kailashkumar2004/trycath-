0d74de996c9cd8eb13bbc5b649dfe303a430522b
kailash <kailashkumartkg@gmail.com>
2023-07-27 21:52:01 +0530

    initial commit

---
+++ try.catch.js
@@ -0,0 +1,138 @@
+
+// try {
+//     console.log("we are inside th try blocks")
+//     fun()
+
+// } catch (error) {
+//     console.log("i am fine", error)
+// }
+
+// var num1 = 901
+// var num2 = 90
+// try {
+//     if (num1 > num2) {
+//         console.log("you have done")
+
+//     }
+//     add()
+// } catch (error) {
+//     console.log("you have done",error)
+// }
+
+// var a = 9
+// var b = 6
+// var c = 11
+// try {
+//     if (a == b && b < c) {
+//         console.log("condation is a ture")
+//     }
+//     else if (a > b && b > c) {
+//         console.log("condation is a not ture")
+//     }
+//     else if (a > b && b < c) {
+//         console.log("i am kailash")
+//     }
+//     func()
+// } catch (error) {
+//     console.log("you are okk",error)
+// }
+
+
+// var readline = require("readline-sync")
+// var num = readline.questionInt("enter the any num==============")
+// var i = 1
+// try {
+//     while (i <= num) {
+//         if (i % 3 == 0 && i % 7 == 0) {
+//             console.log(i,"navgurukul")
+//         }
+//         else if (i % 7 == 0) {
+//             console.log(i,"gurukul")
+//         }
+//         else if (i % 3 == 0) {
+//             console.log(i,"nav")
+//         }
+//         else {
+//             console.log(i)
+//         }
+//         i=i+1
+//     }
+//     kailash()
+// } catch (error) {
+//     console.log("this is a error",error)
+// }
+
+// var array = [67, 89, 75, 32, 13, 70, 90, 41];
+// var sum = 0
+// try {
+//     for (var i in array) {
+//         sum=sum+array[i]
+//     }
+//     console.log("total of sum")
+
+//    num()
+// } catch (error) {
+//     console.log("throw",error)
+// }
+
+
+
+// var num1 = 90
+// var num2 = 10
+// try {
+//     if (num1 >num2) {
+//         console.log("num1 bada hai num2 se")
+//     }
+//     else if (num1 < num2) {
+//         console.log("num2 bada hai num1 se")
+//     }
+//     num()
+// } catch (error) {
+//     console.log("throw=========================",error)
+// }
+
+// try {
+//     var readline = require("readline-sync")
+//     var num = readline.questionInt("enter the any num=================");
+//     var i = 2
+//     var count = 0
+//     while (i <= num) {
+//         if (num % i == 0) {
+//             count=count+1
+//         }
+//         i=i+1
+//     }
+//     console.log("count==========================", count)
+//     if (count == 1) {
+//         console.log(count,"count is a prime number")
+//     }
+//     else {
+//         console.log("count is a not prime number")
+//     }
+//     func()
+// } catch (error) {
+//     console.log("throw",error)
+// }
+
+// try {
+//     var readline = require("readline-sync");
+//     var num = readline.questionInt("enter the any num=================");
+//     var i = 1;
+//     var sum = 0;
+//     while (i < num) {
+//         if (num % i == 0) {
+//             sum=sum+i
+//         }
+//         i=i+1
+//     }
+//     if (sum == num) {
+//         console.log(sum,"sum is a perfect number")
+//     }
+//     else {
+//         console.log(sum,"sum is a not perfect number")
+//     }
+//     kailash()
+// } catch (error) {
+//     console.log("throw",error)
+    
+// }
